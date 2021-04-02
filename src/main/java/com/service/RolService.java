b11e8f2585162de82c261478a09a5722080be0ab
Phantom <Phantom@DESKTOP-920PA8E>
2021-04-02 18:13:36 -0300

    Oauth backend complete

---
+++ src/main/java/com/service/RolService.java
@@ -0,0 +1,30 @@
+package com.service;
+
+import com.entity.Rol;
+import com.enums.RolNombre;
+import com.repository.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RolService {
+
+    @Autowired
+    RolRepository rolRepository;
+
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return rolRepository.findByRolNombre(rolNombre);
+    }
+
+    public boolean existsRolNombre(RolNombre rolNombre){
+        return rolRepository.existsByRolNombre(rolNombre);
+    }
+
+    public void save(Rol rol){
+        rolRepository.save(rol);
+    }
+}
